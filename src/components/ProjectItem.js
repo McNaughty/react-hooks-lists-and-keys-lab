a92baa54b068e31e6e3b415bec3644e6e07f6fca
Winnie <wimacoj@gmail.com>
2023-11-23 11:31:34 +0300

    Completed assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,23 @@
 import React from "react";
+import {name, about, technologies} from "./ProjectList";
 
-function ProjectItem({ name, about, technologies }) {
+function ProjectItem({name,about,technologies}) {
+  // const techOPt = technologies.map((techItem) =>{
+    
+  //   return  <div key={techItem.id}>{techItem}</div>
+   
+  // })
+
   return (
     <div className="project-item">
-      <h3>{name}</h3>
-      <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        <h3>{name}</h3>
+        <p>{about}</p>
+        {technologies.map((technology, index) => {
+          //return (<span key={technology+'-'+index}>{technology}</span>); 
+          return (<span key={technology}>{technology}</span>)
+        })}
+        
       </div>
     </div>
   );
